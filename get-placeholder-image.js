e688217f4846d52a71cef68a2d1f6f1cbdda637d
alecrios <alecrios@utexas.edu>
2017-03-28 00:48:58 -0500

    changed prompt message

--- get-placeholder-image.js
+++ get-placeholder-image.js
@@ -2,7 +2,7 @@
 // Generates a placeholder image with custom dimensions via https://unsplash.it/
 
 javascript: (function() {
-	var size = window.prompt('Enter Image Size (e.g. 1920x1080)', '1920x1080');
+	var size = window.prompt('Enter Image Size', '1920x1080');
 	if (size !== null && size.length) {
 		size = size.toLowerCase().replace(/\s+/g, '').split('x', 2);
 		window.open(`https://unsplash.it/${size[0]}/${size[1]}/?random`);
