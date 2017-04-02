b7a5ffacac6179c5e729da436e9fb464134c46b6
alecrios <alecrios@utexas.edu>
2017-04-01 21:27:49 -0500

    updated to use window location origin

--- search-current-website/search-current-website.js
+++ search-current-website/search-current-website.js
@@ -2,9 +2,8 @@
 // Searches Google for the specified search term within the current domain
 
 javascript: (function() {
-	var site = /\/([^\s\/]+)/g.exec(window.location)[1];
 	var query = encodeURIComponent(window.prompt('Enter Search Query', ''));
 	if (query !== null && query.length) {
-		window.open(`https://encrypted.google.com/search?q=site:${site}%20${query}`);
+		window.open(`https://encrypted.google.com/search?q=site:${window.location.origin}%20${query}`);
 	}
 })();
