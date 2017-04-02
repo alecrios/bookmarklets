43b2c15af106ca811a1ee89ba6c9321dbea5d694
alecrios <alecrios@utexas.edu>
2017-04-02 01:54:13 -0500

    fixed issue with returning null

--- search-current-website/search-current-website.js
+++ search-current-website/search-current-website.js
@@ -2,8 +2,8 @@
 // Searches Google for the specified search term within the current domain
 
 javascript: (function() {
-	var query = encodeURIComponent(window.prompt('Enter Search Query', ''));
+	var query = window.prompt('Enter Search Query', '');
 	if (query !== null && query.length) {
-		window.open(`https://encrypted.google.com/search?q=site:${window.location.origin}%20${query}`);
+		window.open('https://encrypted.google.com/search?q=site:' + window.location.origin + '%20' + encodeURIComponent(query));
 	}
 })();
