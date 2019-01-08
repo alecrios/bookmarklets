b86dc8231e6015ffcf67be5592857568e438abd2
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-01-08 15:26:49 -0600

    Refactored code

--- src/show-element-outlines.js
+++ src/show-element-outlines.js
@@ -4,43 +4,15 @@ javascript: (function() {
 	if (typeof showElementOutlines.isActive === 'undefined') {
 		showElementOutlines.isActive = false;
 		showElementOutlines.style = document.createElement('style');
-		showElementOutlines.style.innerHTML = '*:not(#element-overlay) {outline: 1px solid rgba(255,0,0,.8) !important;}';
-		showElementOutlines.overlay = document.createElement('div');
-		showElementOutlines.overlay.setAttribute('id', 'element-overlay');
-		showElementOutlines.overlay.style.backgroundColor = 'rgba(255,0,0,.2)';
-		showElementOutlines.overlay.style.zIndex = '100000000';
-		showElementOutlines.overlay.style.position = 'absolute';
-		showElementOutlines.overlay.style.top = '0';
-		showElementOutlines.overlay.style.left = '0';
-		showElementOutlines.overlay.style.width = '1px';
-		showElementOutlines.overlay.style.height = '1px';
-		showElementOutlines.overlay.style.transformOrigin = '0 0';
-		showElementOutlines.overlay.style.pointerEvents = 'none';
-		showElementOutlines.overlay.style.opacity = '0';
-		showElementOutlines.updateOverlay = (event) => {
-			if (!event.metaKey) {
-				showElementOutlines.overlay.style.opacity = '0';
-				showElementOutlines.overlay.style.transform = '';
-				return;
-			};
-
-			let rect = event.target.getBoundingClientRect();
-			showElementOutlines.overlay.style.transform = `translateX(${rect.left}px) translateY(${rect.top}px) scaleX(${rect.width}) scaleY(${rect.height})`;
-			showElementOutlines.overlay.style.opacity = '1';
-		}
+		showElementOutlines.style.innerHTML = '* {outline: 1px solid red !important}';
 	}
 
 	if (showElementOutlines.isActive) {
 		showElementOutlines.isActive = false;
 		document.head.removeChild(showElementOutlines.style);
-		document.body.removeChild(showElementOutlines.overlay);
-		document.removeEventListener('mousemove', showElementOutlines.updateOverlay);
 		return;
 	}
 
 	showElementOutlines.isActive = true;
 	document.head.appendChild(showElementOutlines.style);
-	document.body.appendChild(showElementOutlines.overlay);
-	document.addEventListener('mousemove', showElementOutlines.updateOverlay);
-
 })();
--- src/show-window-size.js
+++ src/show-window-size.js
@@ -1,14 +1,15 @@
 javascript: (function() {
 	window.showWindowSize = window.showWindowSize || {};
+
 	if (!window.showWindowSize.isActive) {
 		window.showWindowSize.isActive = true;
 		window.showWindowSize.originalTitle = document.title;
 		window.showWindowSize.update = () => {document.title = `${window.innerWidth} × ${window.innerHeight}`};
 		window.showWindowSize.update();
 		window.addEventListener('resize', window.showWindowSize.update);
-	} else {
-		window.showWindowSize.isActive = false;
-		document.title = window.showWindowSize.originalTitle;
-		window.removeEventListener('resize', window.showWindowSize.update);
 	}
+
+	window.showWindowSize.isActive = false;
+	document.title = window.showWindowSize.originalTitle;
+	window.removeEventListener('resize', window.showWindowSize.update);
 })();
