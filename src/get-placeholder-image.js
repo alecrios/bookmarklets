08f36c5c1c58cdce559de90695a57f18db540287
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-24 17:20:55 -0500

    Updated to accommodate API change

--- src/get-placeholder-image.js
+++ src/get-placeholder-image.js
@@ -1,9 +1,10 @@
 javascript: (function() {
 	let isRepeatRequest = window.location.host === 'picsum.photos';
 
-	let previousDimensions = window.location.pathname.split('/');
-	let previousWidth = isRepeatRequest ? previousDimensions[1] : null;
-	let previousHeight = isRepeatRequest ? previousDimensions[2] : null;
+	let splitPathName = window.location.pathname.split('/');
+	let previousDimensions = splitPathName.slice(splitPathName.length - 2, splitPathName.length);
+	let previousWidth = isRepeatRequest ? previousDimensions[0] : null;
+	let previousHeight = isRepeatRequest ? previousDimensions[1] : null;
 
 	let defaultValue = `${previousWidth || 1920}x${previousHeight || 1080}`;
 
@@ -20,7 +21,7 @@ javascript: (function() {
 		return;
 	}
 
-	let url = `https://picsum.photos/${width}/${height}/?random`;
+	let url = `https://picsum.photos/${width}/${height}`;
 
 	isRepeatRequest ? window.location = url : window.open(url);
 })();
